34ecdb7b8fecf24416cde00ec381cdde5561f2cb
Ryan Rushton <ryan.rushton79@gmail.com>
2019-06-18 22:32:50 +1000

    Made tech icon popups. Sass'd some css and updated some packages.

--- src/intro/Greeting.jsx
+++ src/intro/Greeting.jsx
@@ -21,6 +21,7 @@ import {
 } from "@fortawesome/free-brands-svg-icons";
 import "./Greeting.scss";
 import meCropped from "../static/me_cropped.png";
+import Icon from "./Icon";
 
 const roundPortrait = (
     <img className="rr-greeting-portrait" src={meCropped} alt="" />
@@ -43,27 +44,83 @@ const github = (
 
 const techIcons = [
     [
-        { icon: faJs, title: "JS" },
-        { icon: faReact, title: "React" },
-        { icon: faJava, title: "Java" },
-        { icon: faHtml5, title: "HTML" },
-        { icon: faCss3, title: "CSS" }
+        {
+            icon: faJs,
+            text:
+                "JS. What more can I say, every engineer that loves frontend loves JS."
+        },
+        {
+            icon: faReact,
+            text:
+                "React. My preference of the modern JS frameworkds. Components make things so much tidier."
+        },
+        {
+            icon: faJava,
+            text:
+                "Java. The backend language that powers enterprise. I use this at work and it pays my bills."
+        },
+        { icon: faHtml5, text: "HTML. Can't build websites without it." },
+        {
+            icon: faCss3,
+            text:
+                "CSS. The tech every dev loves to hate. I've been making it my mission to appreciate it's power."
+        }
     ],
     [
-        { icon: faPython, title: "Python" },
-        { icon: faSass, title: "Sass" },
-        { icon: faApple, title: "Apple" },
-        { icon: faLinux, title: "Linux" },
-        { icon: faMicrosoft, title: "VS Code\nMSSQL" }
+        {
+            icon: faPython,
+            text:
+                "Python. My personal favourite language. It can do all sorts of wonderful things and I wish I had more time to builds applications with it."
+        },
+        {
+            icon: faSass,
+            text: "Sass. This makes CSS so much nicer to deal with."
+        },
+        {
+            icon: faApple,
+            text:
+                "Apple. Been using macs for years now. I love how easy they are to use."
+        },
+        {
+            icon: faLinux,
+            text:
+                "Linux. The power of linux is amazing. It's so flexible and I use it everyday but I will never know it's darkest secrets."
+        },
+        {
+            icon: faMicrosoft,
+            text:
+                "Microsoft VS Code and MSSQL. I work faster with Code and use MSSQL at work. I also have to build websites with IE in mind but lets not talk about that."
+        }
     ],
     [
-        { icon: faDocker, title: "Docker" },
-        { icon: faAws, title: "AWS" },
-        { icon: faFontAwesome, title: "Font Awesome" },
-        { icon: faGithub, title: "GitHub" },
-        { icon: faUikit, title: "UIKit" }
+        {
+            icon: faDocker,
+            text:
+                "Docker. Containers rule! All my db's live in one these days. If only it wasn't so hungry on a mac."
+        },
+        {
+            icon: faAws,
+            text:
+                "AWS. I use this for deployments at work. Makes my life easier."
+        },
+        {
+            icon: faFontAwesome,
+            text:
+                "Font Awesome. Stylish icons with little fuss. These are font awesome icons. Let's face it I'm no designer."
+        },
+        { icon: faGithub, text: "GitHub. Host all my projects." },
+        {
+            icon: faUikit,
+            text: "UIKit. Use this at work to build consistent UI's."
+        }
     ],
-    [{ icon: faAtlassian, title: "Jira\nBitBucket\nConfluence\nSourceTree" }]
+    [
+        {
+            icon: faAtlassian,
+            text:
+                "Atlassians Jira, BitBucket, Confluence and SourceTree. Great project tools I use everyday. The pride and joy of Australian software companies."
+        }
+    ]
 ];
 
 const greetingText = (
@@ -83,19 +140,11 @@ const greetingText = (
     </div>
 );
 
-const buildGridItem = iconSet => (
-    <div
-        className="rr-tech-grid-item"
-        key={iconSet.icon.iconName}
-        title={iconSet.title}
-    >
-        <FontAwesomeIcon icon={iconSet.icon} />
-    </div>
-);
-
 const buildGridRow = (row, index) => (
     <div className="rr-tech-grid-row" key={`tech-grid-row-${index}`}>
-        {row.map(icon => buildGridItem(icon))}
+        {row.map(iconSet => (
+            <Icon icon={iconSet.icon} text={iconSet.text} />
+        ))}
     </div>
 );
 
