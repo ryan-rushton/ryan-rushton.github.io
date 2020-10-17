571df37ea373ea571c6a63741bbfc1afcab00a0e
Ryan Rushton <ryan.rushton79@gmail.com>
2020-10-17 14:38:30 +1100

    Fixed navbar styling

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -9,33 +9,31 @@ const LOGO_WIDTH = 70;
 
 const NavBar: FC = () => {
   return (
-    <>
-      <div className={styles.navbar}>
-        <div className={styles.content}>
-          <div className={styles.flexItem}>
-            <a href="/" className={`${styles.link} ${styles.homeLink}`}>
-              <img
-                className={styles.logo}
-                src={'/RR_Logo_Outlined.svg'}
-                width={LOGO_WIDTH}
-                height={LOGO_HEIGHT}
-                alt=""
-              />
+    <div className={styles.navbar}>
+      <div className={styles.content}>
+        <div className={styles.flexItem}>
+          <a href="/" className={`${styles.link} ${styles.homeLink}`}>
+            <img
+              className={styles.logo}
+              src={'/RR_Logo_Outlined.svg'}
+              width={LOGO_WIDTH}
+              height={LOGO_HEIGHT}
+              alt=""
+            />
+          </a>
+        </div>
+        <div className={styles.flexItem}>
+          <div className={styles.rightLinks}>
+            <a href="https://github.com/rushton3179/personal-site" className={styles.link}>
+              <FontAwesomeIcon icon={faGithub} />
             </a>
-          </div>
-          <div className={styles.flexItem}>
-            <div className={styles.rightLinks}>
-              <a href="https://github.com/rushton3179/personal-site" className={styles.link}>
-                <FontAwesomeIcon icon={faGithub} />
-              </a>
-              <a href="mailto:ryan.rushton79@gmail.com" className={styles.link}>
-                <FontAwesomeIcon icon={faEnvelope} />
-              </a>
-            </div>
+            <a href="mailto:ryan.rushton79@gmail.com" className={styles.link}>
+              <FontAwesomeIcon icon={faEnvelope} />
+            </a>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
