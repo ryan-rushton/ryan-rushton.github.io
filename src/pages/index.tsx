df358448f9ef515ba2888e0ac360d22a122ca100
Ryan Rushton <ryan.rushton79@gmail.com>
2021-01-16 00:09:35 +1100

    Moved google verification tag position

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,16 +25,16 @@ const App: FC = () => {
       <>
         <Head>
           <title>{title}</title>
+          <meta
+            name="google-site-verification"
+            content="aS1PdJaaTNWeCb2FTM8-R5DFfDZu9kBEdOoGDSKRE1w"
+          />
           <link rel="icon" type="image/svg" href="/RR_Logo_Outlined.svg" />
           <meta name="description" content={description} />
           <meta property="og:type" content="website" />
           <meta property="og:title" content={title} />
           <meta property="og:description" content={description} />
           <meta property="og:site_name" content={title} />
-          <meta
-            name="google-site-verification"
-            content="aS1PdJaaTNWeCb2FTM8-R5DFfDZu9kBEdOoGDSKRE1w"
-          />
         </Head>
         <NavBar />
         <Greeting />
